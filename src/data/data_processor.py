d8d4394bb6611c26efef310f00be15879b739196
Esteban <estebanvazquez@live.com>
2025-01-10 00:51:20 -0300

    trained model

--- src/data/data_processor.py
+++ src/data/data_processor.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
-def process_data(df: pd.DataFrame, columns_to_impute: list, target_column: str = None) -> pd.DataFrame:
+def process_data(df: pd.DataFrame, columns_to_impute: list, target_column: str = None) -> tuple[pd.DataFrame, pd.Series]:
     """
     Procesa los datos:
     - Imputa los valores faltantes
--- src/main.py
+++ src/main.py
@@ -4,44 +4,42 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from src.data.data_loader import load_data
 from src.data.data_processor import process_data
 import pandas as pd
-#from src.data.data_splitter import split_data
-#from src.model.trainer import train_model
-#from src.model.evaluator import evaluate_model
-#from src.model.saver import save_model
+from src.data.data_splitter import split_data
+from src.model.trainer import train_model
+from src.model.evaluator import evaluate_model
+from src.model.saver import save_model
 
 def main():
 
-# Cargar los datos
+# Carga de datos
     data = load_data(file_path = "data/raw/heart_disease.csv")
 
-# Preprocesar los datos
+# Preprocesamiento de datos
     processed_data, target = process_data(
-                                  df=data, columns_to_impute=['trestbps', 'chol', 'thalach', 'oldpeak'],  # columnas con posibles valores NaN
-                                  target_column='num'                                                     # valor objetivo en el dataset
-                                  )
+                                        df=data, columns_to_impute=['trestbps', 'chol', 'thalach', 'oldpeak'],  # columnas con posibles valores NaN
+                                        target_column='num'                                                     # valor objetivo en el dataset
+                                        )
 
-# Dividir los datos en conjuntos de entrenamiento y prueba
+# Conjuntos de entrenamiento y prueba
     X_train, X_test, y_train, y_test = split_data(processed_data, target_column='num')
 
-# Convertir 'y' en binario
     y_train_binary = (y_train > 0).astype(int)
     y_test_binary = (y_test > 0).astype(int)
 
-# Entrenar el modelo
+# Entrenamiento del modelo
     model = train_model(X_train=X_train, y_train=y_train_binary)
 
-# Evaluar el modelo
+# Evaluaacion del modelo
     accuracy, precision, recall, f1, auc = evaluate_model(model, test_data=X_test, y_test=y_test_binary)
 
-# Impresion de las metricas
+# Impresion de metricas resultantes
     print(f"Accuracy: {accuracy:.2f}")
     print(f"Precision: {precision:.2f}")
     print(f"Recall: {recall:.2f}")
     print(f"F1: {f1:.2f}")
     print(f"AUC: {auc:.2f}")
 
-
-# Guardado del modelo
+# Model saver
     save_model(model, model_path="models/trained_model")
 
 if __name__ == "__main__":
